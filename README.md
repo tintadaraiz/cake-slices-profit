deeca94f9b7b94d913bb54edd700d98beda67388
rodrigo <rd810silva@gmail.com>
2023-03-17 11:44:54 +0000

    update

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# cake-slices-profit
- Desc
+# B: quatre_quarts
+
+---
+
+## noções importantes:
+
+→ o primeiro inteiro que introduzimos é o nº de fatias máximo, portanto, n ≤ m, sendo m o segundo inteiro que introduzimos, para dar número ao ciclo (para adicionar os preços)
+
+---
+
+# roadmap
+
+[https://github.com/tintadaraiz/cake-slices-profit](https://github.com/tintadaraiz/cake-slices-profit)
+
+→ repositório
+
+---
+
+- **funções:**
+    
+    ```ocaml
+    let n = read_int()
+    let m = read_int()
+    (com m <= n)
+    
+    let htbl = Hashtbl.create m
+    
+    let adc m =
+    ```
+    
+
+---
+
+[If Statements, Loops and Recursions · OCaml Tutorials](https://ocaml.org/docs/if-statements-and-loops)
--- quatre_quarts/bin/main.ml
+++ quatre_quarts/bin/main.ml
@@ -1,1 +1,24 @@
-let () = print_endline "Hello, World!"
+(* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+* ENTRADA *
+* uma linha com um inteiro n *
+* uma linha com o valor m que é o numero de tamanho de fatias considerado na tabela de preço *
+* as restantes m linhas contém dois inteiros i j(separados por um espaço). o inteiro i dá o tamanho da fatia e j é o seu preço*
+
+* SAÍDA *
+* uma primeira linha com o valor inteiro L, o lucro máximo que o pasteleiro consegue com a venda de um só bolo (inteiro) 
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *)
+
+(* linha com inteiro n *)
+let n = read_int()
+
+(* linha com o inteiro m *)
+let m = read_int()
+
+ (* HashTable com o tamanho n*)
+let htbl = Hashtbl.create m
+
+(* ciclo para adicionar o nº de fatias + preço *)
+let rec adc m = 
+    let (num1, num2) = Scanf.scanf "%d %d" (fun a b -> (a,b));
+    Hashtbl.add htbl (num1, num2)
+
