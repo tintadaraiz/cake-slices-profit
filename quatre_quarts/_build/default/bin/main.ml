0077fb5d3ae95158e67ce038a794dc88624d9eee
rodrigo <rd810silva@gmail.com>
2023-05-03 22:01:58 +0100

    update

--- quatre_quarts/_build/default/bin/main.ml
+++ quatre_quarts/_build/default/bin/main.ml
@@ -8,37 +8,42 @@
  * uma primeira linha com o valor inteiro L, o lucro máximo que o pasteleiro consegue com a venda de um só bolo (inteiro)            *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *)
 
-(* lista recursiva *)
-let quatre_quarts_price n m items =
-  let rec solver i j =
-    if i < 0 || j <= 0 then 0
-    else
-      let wn, pn = items.(i) in
-      if wn > j then solver (i - 1) j
-      else max (solver (i - 1) j) (pn + solver (i - 1) (j - wn))
-  in solver (m - 1) n
+(* dynamic programming  *)
+let validate_input n m =
+  if m <= 0 || m > 10000 || n <= 0 || n > 10000 || m > n then
+    failwith "Os valores de M(número de fatias) e N(número de fatias a serem vendidas) devem estar no intervalo 0 < M <= 10000 , 0 < N <= 10000, e M <= N."
+
+let quatre_quarts_price n m slices =
+  let memo = Array.make_matrix (m+1) (n+1) 0 in
+  for i = 1 to m do
+    let fatia, preco = slices.(i-1) in
+    for j = 1 to n do
+      if fatia > j then memo.(i).(j) <- memo.(i-1).(j)
+      else memo.(i).(j) <- max (memo.(i-1).(j)) (preco + memo.(i-1).(j-fatia))
+    done;
+  done;
+  memo.(m).(n)
 
 let () =
   let n, m = Scanf.scanf "%d %d\n" (fun a b -> a, b) in
-  if (m <= 0 || m > 10000 || n <= 0 || n > 10000 || m > n) then
-    failwith "Os valores de M(número de fatias) e N(número de fatias a serem vendidas) devem estar no intervalo 0 < M <= 10000 , 0 < N <= 10000, e M <= N."
-  else
-    let items = Array.init m (fun _ ->
-      let slice, price = Scanf.scanf "%d %d\n" (fun a b -> a, b) in
-      if slice > n then failwith "Número de fatias excedido"
-      else (slice, price))
-    in
-    let max_price = quatre_quarts_price n m items in
-    print_string (string_of_int max_price ^ "\n")
+  validate_input n m;
+  let slices = Array.init m (fun _ ->
+    let fatia, preco = Scanf.scanf "%d %d\n" (fun a b -> a, b) in
+    if fatia > n then failwith "Número de fatias excedido";
+    (fatia, preco))
+  in
+  let max_price = quatre_quarts_price n m slices in
+  print_string (string_of_int max_price ^ "\n");
 
 
 
 (* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
  * INSPIRAÇOES                                                                                                                       *
  * https://www.geeksforgeeks.org/0-1-knapsack-problem-dp-10/                                                                         *
- * https://ocaml.org/docs/lists                                                                                                      *
+ * https://v2.ocaml.org/api/Array.html                                                                                               *
  * https://cs3110.github.io/textbook/chapters/data/lists.html                                                                        *
  * https://rosettacode.org/wiki/Knapsack_problem/0-1                                                                                 *
  * https://medium.com/@fabianterh/how-to-solve-the-knapsack-problem-with-dynamic-programming-eb88c706d3cf                            *
  * https://ilyasergey.net/YSC2229/week-09-dynamic-programming.html                                                                   *
+ * https://dev.realworldocaml.org/imperative-programming.html    Topic Memoization and Dynamic Programming                           *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *)
--- quatre_quarts/bin/main.ml
+++ quatre_quarts/bin/main.ml
@@ -8,39 +8,43 @@
  * uma primeira linha com o valor inteiro L, o lucro máximo que o pasteleiro consegue com a venda de um só bolo (inteiro)            *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *)
 
-(* função solver -> dá o preço máximo *)
-let quatre_quarts_price n m items =
-  let rec solver i j =
-    if i < 0 || j <= 0 then 0 (* j é o número de fatias restantes *)
-    else
-      let sn, pn = items.(i) in 
-      if sn > j then solver (i - 1) j
-      else max (solver (i - 1) j) (pn + solver (i - 1) (j - sn))
-  in solver (m - 1) n (* m - 1 dá-nos o índice do ultimo valor do array *)
-
-(* inicializar o array e dar os valores ao mesmo *)
-let () =
-  let n, m = Scanf.scanf "%d %d\n" (fun a b -> a, b) in (* input -> n: número de fatias; m: número de fatias a serem vendidas *)
-  if (m <= 0 || m > 10000 || n <= 0 || n > 10000 || m > n) then (* verificação *)
+(* dynamic programming  *)
+let validar_input n m =
+  if m <= 0 || m > 10000 || n <= 0 || n > 10000 || m > n then
     failwith "Os valores de M(número de fatias) e N(número de fatias a serem vendidas) devem estar no intervalo 0 < M <= 10000 , 0 < N <= 10000, e M <= N."
-  else
-    let items = Array.init m (fun _ ->
-      let slice, price = Scanf.scanf "%d %d\n" (fun a b -> a, b) in
-      if slice > n then failwith "Número de fatias excedido"
-      else (slice, price))
-    in
-    let max_price = quatre_quarts_price n m items in
-    print_string (string_of_int max_price);
-    print_endline("")
+
+let quatre_quarts_price n m slices =
+  let memo = Array.make_matrix (m+1) (n+1) 0 in
+  for i = 1 to m do
+    let fatia, preco = slices.(i-1) in
+    for j = 1 to n do
+      if fatia > j then memo.(i).(j) <- memo.(i-1).(j)
+      else memo.(i).(j) <- max (memo.(i-1).(j)) (preco + memo.(i-1).(j-fatia))
+    done;
+  done;
+  memo.(m).(n)
+
+let () =
+  let n, m = Scanf.scanf "%d %d\n" (fun a b -> a, b) in
+  validar_input n m;
+  let slices = Array.init m (fun _ ->
+    let fatia, preco = Scanf.scanf "%d %d\n" (fun a b -> a, b) in
+    if fatia > n then failwith "Número de fatias excedido";
+    (fatia, preco))
+  in
+  let max_price = quatre_quarts_price n m slices in
+  print_string (string_of_int max_price ^ "\n");
+  print_endline("")
 
 
 
 (* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
  * INSPIRAÇOES                                                                                                                       *
  * https://www.geeksforgeeks.org/0-1-knapsack-problem-dp-10/                                                                         *
- * https://ocaml.org/docs/lists                                                                                                      *
+ * https://v2.ocaml.org/api/Array.html                                                                                               *
  * https://cs3110.github.io/textbook/chapters/data/lists.html                                                                        *
  * https://rosettacode.org/wiki/Knapsack_problem/0-1                                                                                 *
  * https://medium.com/@fabianterh/how-to-solve-the-knapsack-problem-with-dynamic-programming-eb88c706d3cf                            *
  * https://ilyasergey.net/YSC2229/week-09-dynamic-programming.html                                                                   *
+ * https://dev.realworldocaml.org/imperative-programming.html    Topic Memoization and Dynamic Programming                           *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *)
